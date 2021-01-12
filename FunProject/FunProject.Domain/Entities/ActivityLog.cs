138f6ea22b76ea4d62fb9699a18fffae2382faf9
Roee Baram <roeebaram@gmail.com>
2021-01-12 15:40:56 +0200

    add web api project

---
+++ FunProject/FunProject.Domain/Entities/ActivityLog.cs
@@ -0,0 +1,18 @@
+﻿using FunProject.Domain.Enums;
+using System;
+
+namespace FunProject.Domain.Entities
+{
+    public class ActivityLog
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public ActionType ActionType { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public string CustomerFullName { get; set; }
+        //public Customer Customer { get; set; }
+     
+    }
+
+  
+}
---
+++ FunProject/FunProject.Domain/Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FunProject.Domain.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public ICollection<ActivityLog> ActivityLogs { get; set; }
+
+    }
+}
